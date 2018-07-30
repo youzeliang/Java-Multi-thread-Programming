753223682a013f7c584c525f818d4ecebf376d24
yzrds <youzel@126.com>
2018-07-30 23:50:54 +0800

    补充

--- src/chapter01/section07/thread_1_7_3/project_3_t13_1/MyThread.java
+++ src/chapter01/section07/thread_1_7_3/project_3_t13_1/MyThread.java
@@ -5,7 +5,7 @@ public class MyThread extends Thread {
 	public void run() {
 		super.run();
 		try {
-			for (int i = 0; i < 500000; i++) {
+			for (int i = 0; i < 500; i++) {
 				if (this.interrupted()) {
 					System.out.println("已经是停止状态了!我要退出了!");
 					throw new InterruptedException();
